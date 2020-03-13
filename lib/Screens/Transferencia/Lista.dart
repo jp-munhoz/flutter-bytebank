90342db27f9f0031aa122d0b0a4b13bcb80c6e57
JP <pedro.munhoz98@gmail.com>
2020-03-13 12:38:33 -0300

    Refatoração de Código

---
+++ lib/Screens/Transferencia/Lista.dart
@@ -0,0 +1,77 @@
+import 'package:bytebank/Models/Transferencia.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'Formulario.dart';
+
+const _tituloAppBar = 'Transferencias';
+
+class ListaTransferencias extends StatefulWidget {
+  final List<Transferencia> _transferencias = List();
+
+  @override
+  State<StatefulWidget> createState() {
+    return ListaTransferenciasState();
+  }
+}
+
+class ListaTransferenciasState extends State<ListaTransferencias> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_tituloAppBar),
+      ),
+      body: ListView.builder(
+        itemCount: widget._transferencias.length,
+        itemBuilder: (context, indice) {
+          final transferencia = widget._transferencias[indice];
+          return ItemTransferencia(transferencia);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          final Future<Transferencia> future = Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return FormularioTransferencia();
+              },
+            ),
+          );
+          future.then(
+                (transferenciaRecebida) {
+              _atualiza(transferenciaRecebida);
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  void _atualiza(Transferencia transferenciaRecebida) {
+    setState(
+      () {
+        if (transferenciaRecebida != null) {
+          widget._transferencias.add(transferenciaRecebida);
+        }
+      },
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numeroConta.toString()),
+    ));
+  }
+}
